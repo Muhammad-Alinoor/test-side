e569746cb27fb931658eaafb76ef8e51de0e7d69
Muhammad-Alinoor <alinoor145387@outlook.com>
2024-07-09 10:53:46 +0600

    first-commit

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+const myImage = document.querySelector("img");
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/mojilla-logo.jpeg") {
+        myImage.setAttribute("src", "images/firefox-2-bg-removed.png");
+    }
+    else {
+        myImage.setAttribute("src", "images/mojilla-logo.jpeg")
+    }
+}
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+
+}
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+myButton.onclick = () => {
+    setUserName();
+};
